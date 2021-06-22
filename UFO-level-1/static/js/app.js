09d47cb86665f89482f956363ae77235fc49cfd0
Jeremy Bar <78321429+jeremybar32093@users.noreply.github.com>
2021-06-21 22:21:48 -0400

    Updated app.js; added code to populate data table html from data.js file

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,20 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+//1. Identify table body within which to append data rows
+var tbody = d3.select("tbody")
+
+//2. Loop through data entries to create data table
+data.forEach((tableData) => {
+    // Append table row tags within table body
+    var row = tbody.append("tr");
+    // Loop through values within each data entry
+    Object.entries(tableData).forEach(([key, value]) => {
+        // Append td tag within table row created above
+        var cell = row.append("td");
+        // Add values as text within td tags
+        cell.text(value);
+    }); 
+});
+
