d91b4888272f9a78f17bb15ae3edbf63ab3d059c
Jeremy Bar <78321429+jeremybar32093@users.noreply.github.com>
2021-06-22 00:00:46 -0400

    updated app.js; added functions to perform filtering when event listeners are triggered

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,37 +4,70 @@ var tableData = data;
 // YOUR CODE HERE!
 
 // 1. Identify table body within which to append data rows
-var tbody = d3.select("tbody")
+var tbody = d3.select("tbody");
 
-// 2. Loop through data entries to create initial data table
-data.forEach((tableData) => {
+// 2. Create function for rendering data
+function renderData(data) {
+  data.forEach((data) => {
     // Append table row tags within table body
     var row = tbody.append("tr");
     // Loop through values within each data entry
-    Object.entries(tableData).forEach(([key, value]) => {
-        // Append td tag within table row created above
-        var cell = row.append("td");
-        // Add values as text within td tags
-        cell.text(value);
-    }); 
-});
+    Object.entries(data).forEach(([key, value]) => {
+      // Append td tag within table row created above
+      var cell = row.append("td");
+      // Add values as text within td tags
+      cell.text(value);
+    });
+  });
+};
 
-// 3. For event handlers, select form and button html elements
+// 3. Create function for clearing out table rows
+function clearTableRows() {
+  var tableRows = d3.selectAll("#ufo-table tbody tr");
+  tableRows.remove();
+}
+
+
+// 4. Render initial table data by calling function defined above
+renderData(tableData);
+
+// 5. For event handlers, select form and button html elements
 var button = d3.select("#filter-btn");
 var form = d3.select("form");
 
-// 4. Create event handlers 
+// 6. Create event handlers
 // Event listener for clicking "Filter Table" button
 button.on("click", runEnter);
 // Event listener for pressing "Enter" after updating input form
 form.on("submit", runEnter);
 
-// 5. Create function to execute when event listeners are triggered
+// 7. Create function to execute when event listeners are triggered
 function runEnter() {
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
 
-    console.log("Event triggered")
+  // Select date value from form input
+  var date = d3.select("#datetime").property("value");
+
+  console.log(date);
+
+  // Apply event listener logic only if date is event is triggered 
+  // If date is non-null, return filtered value based on date input
+  if (date) {
+    // Clear out td tags within tbody - call function defined above
+    clearTableRows();
+    // Get filtered data to be rendered
+    var filteredData = tableData.filter((row) => row.datetime === date);
+    console.log(filteredData);
+    // Call renderData function for only filteredData
+    renderData(filteredData);
+  } 
+    // If date is null, rerender full dataset
+    else {
+    // Clear out td tags within tbody - call function defined above
+    clearTableRows();
+    // Rerender original full dataset
+    // Call renderData function with tableData variable defined above
+    renderData(tableData);
+  };
 };
-
-
