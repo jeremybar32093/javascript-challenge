f2e7277306066344376e5e95e1f865211f7c9d33
Jeremy Bar <78321429+jeremybar32093@users.noreply.github.com>
2021-06-21 22:51:47 -0400

    Updated app.js; added event listeners for filter form and placeholder function to be triggered by listener

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,10 +3,10 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
-//1. Identify table body within which to append data rows
+// 1. Identify table body within which to append data rows
 var tbody = d3.select("tbody")
 
-//2. Loop through data entries to create data table
+// 2. Loop through data entries to create initial data table
 data.forEach((tableData) => {
     // Append table row tags within table body
     var row = tbody.append("tr");
@@ -18,4 +18,23 @@ data.forEach((tableData) => {
         cell.text(value);
     }); 
 });
+
+// 3. For event handlers, select form and button html elements
+var button = d3.select("#filter-btn");
+var form = d3.select("form");
+
+// 4. Create event handlers 
+// Event listener for clicking "Filter Table" button
+button.on("click", runEnter);
+// Event listener for pressing "Enter" after updating input form
+form.on("submit", runEnter);
+
+// 5. Create function to execute when event listeners are triggered
+function runEnter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    console.log("Event triggered")
+};
+
 
